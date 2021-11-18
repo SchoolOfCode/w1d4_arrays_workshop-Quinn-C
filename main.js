924e468a9e4ef81783f0bfc7390d20e105c3e31e
Quinn.C <joanchenuk@gmail.com>
2021-11-18 15:25:58 +0000

    task1 finished

---
+++ main.js
@@ -0,0 +1,18 @@
+//Task1
+
+let favoriteSongs = ["Tiny Dancer", "Running Up That Hill", "Don't Stop Moving", "Tragedy", "Crazy Horses"];
+
+let profit = [1, 2, 4, 8, 16, 32, 64];
+
+let team = [
+    { name: "Ben", special_skill: "code rapping" },
+    { name: "Tao", special_skill: "hand raising" },
+    { name: "Lizzie", special_skill: "being friendly" },
+    { name: "Liz", special_skill: "crochet" },
+    { name: "Patrick", special_skill: "Call of Duty" },
+    { name: "Chris", special_skill: "changing lives" },
+    { name: "Tim", special_skill: "analogies" },
+    { name: "Joseph", special_skill: "being positive" },
+    { name: "Max", special_skill: "papier-mâché" },
+
+];
