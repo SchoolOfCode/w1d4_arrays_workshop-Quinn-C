54e278f246c4781074580342161a2588115904e8
Quinn.C <joanchenuk@gmail.com>
2021-11-18 15:37:49 +0000

    task3 finished

--- main.js
+++ main.js
@@ -36,3 +36,15 @@ let options = [
 console.log(options[3]);
 console.log(options[6]);
 console.log(options[7]);
+
+//task3
+
+let englishNumbers = ["one", "two", "three", "four", "five"];
+
+let index = 0;
+
+while (index < englishNumbers.length) {
+    console.log(englishNumbers[index]);
+    index++;
+
+}
